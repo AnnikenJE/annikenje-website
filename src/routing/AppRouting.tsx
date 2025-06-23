f40fdcb4367d88cf5c493534bfb5e59012b26e6b
Anniken Jørgensen Edvardsen <annikenje@gmail.com>
2025-06-23 15:46:47 +0200

    added folders and robots

---
+++ src/routing/AppRouting.tsx
@@ -0,0 +1,8 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+const AppRouting = () => {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+};
