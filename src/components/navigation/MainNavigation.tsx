bb14d9d8eadc611df088e96b0fa8598e1e27a218
Anniken Jørgensen Edvardsen <annikenje@gmail.com>
2025-07-31 15:56:02 +0200

    color changes + responsive navigation bar fixed

--- src/components/navigation/MainNavigation.tsx
+++ src/components/navigation/MainNavigation.tsx
@@ -15,39 +15,43 @@ const MainNavigation = () => {
         </Link>
       </div>
       <ul className="main-navigation__list">
-        <li>
-          <Link to="/about" className="main-navigation__item">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="/projects" className="main-navigation__item">
-            Projects
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" className="main-navigation__item">
-            Contact
-          </Link>
-        </li>
-        <li className="social-link">
-          <a
-            href="https://github.com/AnnikenJE"
-            target="_blank"
-            className="main-navigation__item"
-          >
-            <FontAwesomeIcon icon={faSquareGithub} />
-          </a>
-        </li>
-        <li className="social-link">
-          <a
-            href="https://www.linkedin.com/in/anniken-j%C3%B8rgensen-edvardsen-825160113/"
-            target="_blank"
-            className="main-navigation__item"
-          >
-            <FontAwesomeIcon icon={faSquareLinkedin} />
-          </a>
-        </li>
+        <div className="nav-item__wrapper">
+          <li>
+            <Link to="/about" className="main-navigation__item">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects" className="main-navigation__item">
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="main-navigation__item">
+              Contact
+            </Link>
+          </li>
+        </div>
+        <div className="social-item__wrapper">
+          <li className="social-link">
+            <a
+              href="https://github.com/AnnikenJE"
+              target="_blank"
+              className="main-navigation__item"
+            >
+              <FontAwesomeIcon icon={faSquareGithub} />
+            </a>
+          </li>
+          <li className="social-link">
+            <a
+              href="https://www.linkedin.com/in/anniken-j%C3%B8rgensen-edvardsen-825160113/"
+              target="_blank"
+              className="main-navigation__item"
+            >
+              <FontAwesomeIcon icon={faSquareLinkedin} />
+            </a>
+          </li>
+        </div>
       </ul>
     </nav>
   );
