1070dee421ecbe7cc00f7a8c490af7b58a8e237f
Anniken JE <AnnikenJE@gmail.com>
2026-02-20 00:56:02 +0100

    Revert "Add about page html structure"
    This reverts commit 9a7c960f9c382ee092c83d41624ef3f2ad2c4cf9.

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -6,29 +6,11 @@ const AboutPage = () => {
       <section className="about-section">
         <h1 className="about-heading">About</h1>
       </section>
-
       <section>
-        <h2>Who am I?</h2>
-        <p>
-          A student currently pursuing a Bachelor's degree in Frontend and
-          Mobile Development.
-        </p>
-        <p>More information coming soon.</p>
-      </section>
-
-      <section>
-        <h2>Skills</h2>
-        <p>More information coming soon.</p>
-      </section>
-
-      <section>
-        <h2>This Website</h2>
-        <p>
-          This website is an ongoing project where I experiment, learn, and
-          improve over time.
-        </p>
-        <p>More information coming soon.</p>
-      </section>
+      <p>
+         
+      </p>
+         </section>
     </main>
   );
 };
