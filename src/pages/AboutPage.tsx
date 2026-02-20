9f25cfd3d30e1a709443f305df97ac1bf753faf7
Anniken JE <AnnikenJE@gmail.com>
2026-02-20 01:03:53 +0100

    Fix about page html structure

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,15 +2,33 @@ import "./AboutPage.css";
 
 const AboutPage = () => {
   return (
-    <main>
+ <main>
       <section className="about-section">
         <h1 className="about-heading">About</h1>
       </section>
+
       <section>
-      <p>
-         
-      </p>
-         </section>
+        <h2>Who am I?</h2>
+        <p>
+          A student currently pursuing a Bachelor's degree in Frontend and
+          Mobile Development.
+        </p>
+        <p>More information coming soon.</p>
+      </section>
+
+      <section>
+        <h2>Skills</h2>
+        <p>More information coming soon.</p>
+      </section>
+
+      <section>
+        <h2>This Website</h2>
+        <p>
+          This website is an ongoing project where I experiment, learn, and
+          improve over time.
+        </p>
+        <p>More information coming soon.</p>
+      </section>
     </main>
   );
 };
